e25a9c76b9e3c5f0a5334d9b622575b26c72b147
sandhi1x <sandhiyax_balakrishnan@mcafee.com>
2018-01-24 17:33:13 +0530

    {# Changed data ]

--- Common/DB/Mysql/Gti_DB.go
+++ Common/DB/Mysql/Gti_DB.go
@@ -16,9 +16,8 @@ func InsertDomain(Domain string, Reputation int, Categories []int, source string
 	log.Println("inside")
 	var Count int
 	var Gtiid int
-	var Gtimapid int
 
-	query := "SELECT COUNT(*) FROM GTIDOMAIN WHERE GTIDOMAIN.Domain ='" + Domain + "'"
+	query := "SELECT COUNT(*) FROM GTIDOMAIN WHERE GTIDOMAIN.Domain_name ='" + Domain + "'"
 	row := OpenConnection["GTI"].QueryRow(query)
 	row.Scan(
 		&Count,
@@ -26,12 +25,12 @@ func InsertDomain(Domain string, Reputation int, Categories []int, source string
 	if Count == 0 {
 
 		log.Println("Insert Domain")
-		rowss, err := OpenConnection["GTI"].Exec("insert into GTIDOMAIN (Domain,Reputation,Src) values (?,?,?)", Domain, Reputation, source)
+		rowss, err := OpenConnection["GTI"].Exec("insert into GTIDOMAIN (Domain_name,Reputation,Sourcefile) values (?,?,?)", Domain, Reputation, source)
 		if err != nil {
 			log.Println("Error -DB: Profile", err, rowss)
 		}
 
-		row1, err := OpenConnection["GTI"].Query("select DomainId from GTIDOMAIN where Domain =?", Domain)
+		row1, err := OpenConnection["GTI"].Query("select Domain_ID from GTIDOMAIN where Domain_name =?", Domain)
 		if err != nil {
 			log.Println("Error -DB: User", err)
 		}
@@ -46,20 +45,7 @@ func InsertDomain(Domain string, Reputation int, Categories []int, source string
 
 		for i := 0; i < len(Categories); i++ {
 
-			row1, err := OpenConnection["GTI"].Query("select ID from GTI where Category =?", Categories[i])
-			if err != nil {
-				log.Println("Error -DB: User", err)
-			}
-			for row1.Next() {
-
-				row1.Scan(
-
-					&Gtimapid,
-				)
-
-			}
-
-			rows, err := OpenConnection["GTI"].Exec("insert into GTIMAP (DomainID,GTIid) values (?,?)", Gtiid, Gtimapid)
+			rows, err := OpenConnection["GTI"].Exec("insert into GTIDOMAINCATMAPPING (Domain_ID,Category) values (?,?)", Gtiid, Categories[i])
 			if err != nil {
 				log.Println("Error -DB: Executive insert picture", err, rows)
 
@@ -70,7 +56,7 @@ func InsertDomain(Domain string, Reputation int, Categories []int, source string
 
 }
 func InsertGti(Domain string, cat int) {
-	rowss, err := OpenConnection["GTI"].Exec("insert into GTI (Categoryname,Category) values (?,?)", Domain, cat)
+	rowss, err := OpenConnection["GTI"].Exec("insert into GTI (Category,Category_name) values (?,?)", cat, Domain)
 	if err != nil {
 		log.Println("Error -DB: Profile", err, rowss)
 	}
